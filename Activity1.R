9017c262d0347c7563f7ef070f57a851ca5a980f
jamescho27 <jcho@hamilton.edu>
2022-01-27 16:26:30 +0000

    finished hwk1

--- Activity1.R
+++ Activity1.R
@@ -1,6 +1,7 @@
 #prompt 1
-
+CM_CONVERSION <- 2.54
 snow_depth <- c(2.5, 3, 5, 4.5)
+snow_depth <- snow_depth / CM_CONVERSION
 
 #pompt 2
 names <- c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak")
@@ -11,6 +12,7 @@ round_trip <- c(17.8, 17.9, 13.2, 16)
 peaks <- data.frame(name = names, elev = elev, climb = climb, round_trip = round_trip)
 
 METER_CONVERSION <- 3.281
+
 #question 1
 
 peaks[,2] <- peaks[,2] / METER_CONVERSION
